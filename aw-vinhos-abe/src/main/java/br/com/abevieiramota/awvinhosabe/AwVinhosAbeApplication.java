58e1c6fcf96f1f4609dcda66aacf191bdddfa26e
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-02-04 14:17:35 -0200

    2º workshop spring algaworks ~30min aula 2

---
+++ aw-vinhos-abe/src/main/java/br/com/abevieiramota/awvinhosabe/AwVinhosAbeApplication.java
@@ -0,0 +1,12 @@
+package br.com.abevieiramota.awvinhosabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwVinhosAbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwVinhosAbeApplication.class, args);
+	}
+}
