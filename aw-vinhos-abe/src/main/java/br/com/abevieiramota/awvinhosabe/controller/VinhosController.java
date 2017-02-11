5a90c8abb4e4d86b691ebf4bdb6bd3b658b376f3
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-02-11 14:07:30 -0200

    video 3 aw ~26:00

--- aw-vinhos-abe/src/main/java/br/com/abevieiramota/awvinhosabe/controller/VinhosController.java
+++ aw-vinhos-abe/src/main/java/br/com/abevieiramota/awvinhosabe/controller/VinhosController.java
@@ -7,19 +7,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.com.abevieiramota.awvinhosabe.Vinhos;
 import br.com.abevieiramota.awvinhosabe.model.Vinho;
 import br.com.abevieiramota.awvinhosabe.model.Vinho.Tipo;
+import br.com.abevieiramota.awvinhosabe.repository.Vinhos;
+import br.com.abevieiramota.awvinhosabe.repository.filter.VinhoFilter;
 
 @Controller
+@RequestMapping("/vinho")
 public class VinhosController {
 
 	@Autowired
 	private Vinhos vinhoRepository;
 
-	@GetMapping("/vinho/novo")
+	@GetMapping("/novo")
 	public ModelAndView novo(Vinho vinho) {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("tipos", Tipo.values());
@@ -28,13 +32,24 @@ public class VinhosController {
 		return mv;
 	}
 
-	@PostMapping("/vinho/novo")
-	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result) {
+	@GetMapping("/pesquisar")
+	public ModelAndView pesquisar(VinhoFilter vinhoFilter) {
+
+		ModelAndView mv = new ModelAndView("/vinho/pesquisa-vinhos");
+
+		mv.addObject("vinhos", vinhoRepository.findByNomeContainingIgnoreCase(vinhoFilter.getNome()));
+
+		return mv;
+	}
+
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(vinho);
 		}
 
 		vinhoRepository.save(vinho);
+		attributes.addFlashAttribute("mensagem", "Vinho salvo com sucesso!");
 
 		return new ModelAndView("redirect:/vinho/novo");
 	}
--- aw-vinhos-abe/src/main/java/br/com/abevieiramota/awvinhosabe/repository/Vinhos.java
+++ aw-vinhos-abe/src/main/java/br/com/abevieiramota/awvinhosabe/repository/Vinhos.java
@@ -1,8 +1,12 @@
-package br.com.abevieiramota.awvinhosabe;
+package br.com.abevieiramota.awvinhosabe.repository;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.abevieiramota.awvinhosabe.model.Vinho;
 
 public interface Vinhos extends JpaRepository<Vinho, Long> {
+
+	List<Vinho> findByNomeContainingIgnoreCase(String nome);
 }
